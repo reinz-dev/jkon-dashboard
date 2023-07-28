5463626a9ccbacf3043486925eb0ad973bc6b6f0
reinz <reinz.developer@gmail.com>
2023-07-28 18:17:55 +0700

    modified AvatarBox

--- components/AvatarBox.jsx
+++ components/AvatarBox.jsx
@@ -1,7 +1,28 @@
-import { Avatar, IconButton } from "@chakra-ui/react";
+import {
+  Avatar,
+  Button,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverHeader,
+  PopoverArrow,
+  PopoverBody,
+} from "@chakra-ui/react";
 
 export const AvatarBox = ({}) => (
-  <IconButton borderRadius="full">
-    <Avatar name="Ridwan Sulaeman" bg="blue.300" size="xs" />
-  </IconButton>
+  <Popover isLazy>
+    <PopoverTrigger>
+      <Button borderRadius="full" size={1}>
+        <Avatar name="Ridwan Sulaeman" bg="blue.300" size="xs" />
+      </Button>
+    </PopoverTrigger>
+    <PopoverContent bg="blue.900" color="white">
+      <PopoverHeader fontWeight="semibold">Customization</PopoverHeader>
+      <PopoverArrow bg="blue.900" />
+      <PopoverBody>
+        Tadaa!! The arrow color and background color is customized. Check the
+        props for each component.
+      </PopoverBody>
+    </PopoverContent>
+  </Popover>
 );
