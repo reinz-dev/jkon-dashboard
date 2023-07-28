2e4f2cc2ff5ad18418e28b9d6fbc2bd3c7f50941
reinz <reinz.developer@gmail.com>
2023-07-28 17:54:50 +0700

    modified LoginPage

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,25 +1,57 @@
 import { useState } from "react";
-import Login from "../pages/login/LoginPage";
+import { useForm } from "react-hook-form";
+import { toast, ToastContainer } from "react-toastify";
 import Dashboard from "../pages/dashboard/DashboardPage";
+import Login from "../pages/login/LoginPage";
 
 const HomePage = () => {
+  const { register, handleSubmit, errors } = useForm();
+  const [loading, setLoading] = useState(false);
   const [loggedIn, setLoggedIn] = useState(false);
+  const [username, setUsername] = useState("");
 
-  const handleLoginSuccess = () => {
-    setLoggedIn(true);
+  const handleLogin = async (data) => {
+    try {
+      setLoading(true);
+      // Simulasi proses login dengan delay selama 2 detik
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+
+      // Lakukan validasi kredensial di sini (misalnya dengan mengirimkan data ke server)
+      // Gantikan komentar di bawah dengan validasi sesuai dengan username dan password yang benar
+      if (data.username === "username" && data.password === "password") {
+        setLoggedIn(true);
+        setUsername(data.username);
+        toast.success("Login successful!");
+      } else {
+        toast.error("Login failed. Please check your username and password.");
+      }
+
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      toast.error("An error occurred during login.");
+    }
   };
 
   const handleLogout = () => {
+    // Logika untuk logout, seperti menghapus token atau session
+    // Selanjutnya, set loggedIn ke false untuk kembali ke halaman login
     setLoggedIn(false);
   };
 
   return (
     <div>
       {!loggedIn ? (
-        <Login onLoginSuccess={handleLoginSuccess} />
+        <Login
+          handleSubmit={handleSubmit(handleLogin)}
+          register={register}
+          errors={errors}
+          loading={loading}
+        />
       ) : (
-        <Dashboard onLogout={handleLogout} />
+        <Dashboard username={username} handleLogout={handleLogout} />
       )}
+      <ToastContainer />
     </div>
   );
 };
