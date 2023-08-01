87daca787cee748f4ec17be98cf95a1f57182a84
reinz <reinz.developer@gmail.com>
2023-08-01 13:39:18 +0700

    adding modal in dashboard

--- pages/dashboard/DashboardPage.jsx
+++ pages/dashboard/DashboardPage.jsx
@@ -5,9 +5,15 @@ import {
   Text,
   HStack,
   Box,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton,
   useColorModeValue,
 } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/router";
 import { MdMenu } from "react-icons/md";
 import { AiOutlinePoweroff } from "react-icons/ai";
@@ -15,15 +21,25 @@ import { AiOutlineSearch } from "react-icons/ai";
 import { Sidebar } from "../../components/Sidebar";
 import { AvatarBox } from "../../components/AvatarBox";
 import ThemeToggleButton from "../../components/theme-toggle-button";
+import { MotionBox } from "framer-motion";
 
 const DashboardPage = () => {
   const [collapse, setCollapse] = React.useState(false);
   const router = useRouter();
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleLogout = () => {
     // Lakukan proses logout di sini (misalnya dengan menghapus token atau data sesi)
     // Setelah logout berhasil, alihkan ke halaman login
     router.push("../login/LoginPage");
+  };
+
+  const handleOpenModal = () => {
+    setIsOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsOpen(false);
   };
 
   return (
@@ -128,6 +144,8 @@ const DashboardPage = () => {
             align="center"
             rounded={10}
             bg={useColorModeValue("bgLight", "bgDark")}
+            onClick={handleOpenModal}
+            cursor="pointer"
           >
             MAIN
           </Flex>
@@ -162,6 +180,42 @@ const DashboardPage = () => {
       >
         MAIN
       </Flex>
+      <Modal isCentered isOpen={isOpen} onClose={handleCloseModal} size="6xl">
+        <ModalContent bg="transparent" backdropFilter="blur(8px)">
+          <ModalHeader>Modal Judul</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum. Contrary to popular belief, Lorem Ipsum is not simply
+              random text. It has roots in a piece of classical Latin literature
+              from 45 BC, making it over 2000 years old. Richard McClintock, a
+              Latin professor at Hampden-Sydney College in Virginia, looked up
+              one of the more obscure Latin words, consectetur, from a Lorem
+              Ipsum passage, and going through the cites of the word in
+              classical literature, discovered the undoubtable source. Lorem
+              Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus
+              Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero,
+              written in 45 BC. This book is a treatise on the theory of ethics,
+              very popular during the Renaissance. The first line of Lorem
+              Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in
+              section 1.10.32. The standard chunk of Lorem Ipsum used since the
+              1500s is reproduced below for those interested. Sections 1.10.32
+              and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are
+              also reproduced in their exact original form, accompanied by
+              English versions from the 1914 translation by H. Rackham.
+            </p>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </Flex>
   );
 };
