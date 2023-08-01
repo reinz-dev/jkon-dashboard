785f33f71ab5f3ce9add31bad0894541e5facba5
reinz <reinz.developer@gmail.com>
2023-08-01 10:18:33 +0700

    adjust dashboard

--- pages/dashboard/DashboardPage.jsx
+++ pages/dashboard/DashboardPage.jsx
@@ -43,35 +43,122 @@ const DashboardPage = () => {
       >
         <Sidebar collapse={collapse} />
       </Flex>
-      <Box w="full" h="full" gap={3}>
-        <Box w="full" align="right">
-          <IconButton
-            aria-label="Menu Colapse"
-            icon={<AiOutlineSearch />}
-            mr={2}
-            variant="ghost"
-            onClick={() => setCollapse(!collapse)}
-          />
-          <IconButton
-            aria-label="Menu Colapse"
-            icon={<MdMenu />}
-            position="relative"
-            mr={2}
-            variant="ghost"
-            onClick={() => setCollapse(!collapse)}
-          />
-          <IconButton
-            aria-label="Menu Colapse"
-            icon={<AiOutlinePoweroff />}
-            position="relative"
-            mr={2}
-            variant="ghost"
-            onClick={handleLogout}
-          />
-          <ThemeToggleButton />
-          <AvatarBox />
-        </Box>
+      <Box w="full" h="full">
+        <Flex
+          w="full"
+          h="3%"
+          justifyContent="space-between"
+          alignItems="center"
+          rounded={15}
+        >
+          <Box>
+            <IconButton
+              aria-label="Menu Colapse"
+              icon={<MdMenu />}
+              variant="ghost"
+              onClick={() => setCollapse(!collapse)}
+            />
+          </Box>
+          <Box>
+            <IconButton
+              aria-label="Menu Colapse"
+              icon={<AiOutlineSearch />}
+              position="relative"
+              variant="ghost"
+              onClick={() => setCollapse(!collapse)}
+            />
+            <IconButton
+              aria-label="Menu Colapse"
+              icon={<AiOutlinePoweroff />}
+              position="relative"
+              variant="ghost"
+              onClick={handleLogout}
+            />
+            <ThemeToggleButton />
+            <AvatarBox />
+          </Box>
+        </Flex>
+        <Flex w="full" h="3%" align="center" mt="1%" gap={2}>
+          <Flex
+            w="70px"
+            h="full"
+            align="center"
+            justify="center"
+            roundedStart={10}
+            bg={useColorModeValue("#ffffff40", "#00000040")}
+          >
+            LOGO
+          </Flex>
+          <Flex
+            w="full"
+            h="full"
+            align="center"
+            roundedEnd={10}
+            bg={useColorModeValue("#ffffff40", "#00000040")}
+          >
+            MAIN
+          </Flex>
+          <Flex
+            w="20%"
+            h="full"
+            align="center"
+            justify="center"
+            rounded={10}
+            bg={useColorModeValue("#ffffff40", "#00000040")}
+          >
+            MAIN
+          </Flex>
+        </Flex>
+        <Flex w="full" h="50%" mt="1%" gap={3}>
+          <Flex
+            w="300px"
+            justify="center"
+            align="center"
+            rounded={10}
+            bg={useColorModeValue("#ffffff40", "#00000040")}
+          >
+            MAIN
+          </Flex>
+          <Flex
+            w="full"
+            justify="center"
+            align="center"
+            rounded={10}
+            bg={useColorModeValue("#ffffff40", "#00000040")}
+          >
+            MAIN
+          </Flex>
+        </Flex>
+        <Flex w="full" h="40%" mt={2} gap={3}>
+          <Flex
+            w="300px"
+            justify="center"
+            align="center"
+            rounded={10}
+            bg={useColorModeValue("#ffffff40", "#00000040")}
+          >
+            MAIN
+          </Flex>
+          <Flex
+            w="full"
+            justify="center"
+            align="center"
+            rounded={10}
+            bg={useColorModeValue("#ffffff40", "#00000040")}
+          >
+            MAIN
+          </Flex>
+        </Flex>
       </Box>
+      <Flex
+        w="20%"
+        justify="center"
+        align="center"
+        rounded={10}
+        bg={useColorModeValue("#ffffff40", "#00000040")}
+      >
+        MAIN
+      </Flex>
     </Flex>
   );
 };
